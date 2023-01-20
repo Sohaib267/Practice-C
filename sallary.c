b8a0911b76ba8b2b790a54a0d090697f8bb1aa0e
Sohaib267 <120796948+Sohaib267@users.noreply.github.com>
2023-01-20 18:44:05 +0530

    Add files via upload

---
+++ sallary.c
@@ -0,0 +1,23 @@
+/*calculation of gross sallary*/
+#include<stdio.h>
+int main()
+{
+    float bs,gs,da,hra;
+    printf("Enter basic sallary");
+    scanf("%f",&bs);
+    if(bs<1500) 
+    {
+    hra=bs*10/100;
+    da=bs*90/100;
+    }
+    
+        else
+     
+    {
+        hra=500;
+        da=bs*98/100;
+    }
+    gs=bs+hra+da;
+    printf("gross salary=Rs%f\n",gs);
+    return 0;
+}
